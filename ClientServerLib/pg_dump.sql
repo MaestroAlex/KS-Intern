bc980cbc4ffa365ae3fce391881bc157cc9a4217
czAksay <hoba.yurchik@gmail.com>
2019-12-04 08:26:10 +0200

    Final Commit dumo update

--- ClientServerLib/pg_dump.sql
+++ ClientServerLib/pg_dump.sql
@@ -1,37 +1,313 @@
-create table users (
-	id serial primary key,
-	login varchar not null unique,
-	password varchar not null,
-	check (length(login) > 0 AND length(password) > 0)
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.19
+-- Dumped by pg_dump version 9.5.19
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: chatdb; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE chatdb WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'Russian_Russia.1251' LC_CTYPE = 'Russian_Russia.1251';
+
+
+ALTER DATABASE chatdb OWNER TO postgres;
+
+\connect chatdb
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: removeroom(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.removeroom() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN 
+  IF OLD.id=1 THEN RAISE EXCEPTION 'Cant remove General chat room!';
+  ELSE RETURN NEW;
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION public.removeroom() OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: message; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.message (
+    id bigint NOT NULL,
+    user_id integer,
+    room_id integer,
+    date timestamp without time zone DEFAULT now() NOT NULL,
+    message character varying,
+    CONSTRAINT message_message_check CHECK ((length((message)::text) > 0))
 );
-create table room (
-	id serial primary key,
-	name varchar unique,
-	check (length(name) > 0)
+
+
+ALTER TABLE public.message OWNER TO postgres;
+
+--
+-- Name: message_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.message_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.message_id_seq OWNER TO postgres;
+
+--
+-- Name: message_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.message_id_seq OWNED BY public.message.id;
+
+
+--
+-- Name: room; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.room (
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT room_name_check CHECK ((length((name)::text) > 0))
 );
-create table user_in_room (
-	user_id integer references users(id),
-	room_id integer references room(id),
-	primary key (user_id, room_id)
+
+
+ALTER TABLE public.room OWNER TO postgres;
+
+--
+-- Name: room_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.room_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.room_id_seq OWNER TO postgres;
+
+--
+-- Name: room_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.room_id_seq OWNED BY public.room.id;
+
+
+--
+-- Name: user_in_room; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.user_in_room (
+    user_id integer NOT NULL,
+    room_id integer NOT NULL
 );
-create table message (
-	id bigserial primary key,
-	user_id integer references users(id),
-	room_id integer references room(id),
-	date timestamp not null default CURRENT_TIMESTAMP,
-	message varchar check (length(message) > 0)
+
+
+ALTER TABLE public.user_in_room OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    login character varying NOT NULL,
+    password character varying NOT NULL,
+    CONSTRAINT users_check CHECK (((length((login)::text) > 0) AND (length((password)::text) > 0)))
 );
-CREATE OR REPLACE FUNCTION RemoveRoom()
- RETURNS trigger AS
-$BODY$
-BEGIN 
-  IF OLD.id=1 THEN RAISE EXCEPTION 'Cant remove General chat room!';
-  ELSE RETURN NEW;
-  END IF;
-END;
-$BODY$ language plpgsql;
-CREATE TRIGGER removeRoomTrigger
-  BEFORE DELETE ON room
-  FOR EACH ROW
-  EXECUTE PROCEDURE RemoveRoom();
-INSERT INTO room (name) VALUES ('General');
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message ALTER COLUMN id SET DEFAULT nextval('public.message_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.room ALTER COLUMN id SET DEFAULT nextval('public.room_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: message_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: room_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.room
+    ADD CONSTRAINT room_name_key UNIQUE (name);
+
+
+--
+-- Name: room_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.room
+    ADD CONSTRAINT room_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_in_room_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_in_room
+    ADD CONSTRAINT user_in_room_pkey PRIMARY KEY (user_id, room_id);
+
+
+--
+-- Name: users_login_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_login_key UNIQUE (login);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: removeroomtrigger; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER removeroomtrigger BEFORE DELETE ON public.room FOR EACH ROW EXECUTE PROCEDURE public.removeroom();
+
+
+--
+-- Name: message_room_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_room_id_fkey FOREIGN KEY (room_id) REFERENCES public.room(id);
+
+
+--
+-- Name: message_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: user_in_room_room_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_in_room
+    ADD CONSTRAINT user_in_room_room_id_fkey FOREIGN KEY (room_id) REFERENCES public.room(id);
+
+
+--
+-- Name: user_in_room_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_in_room
+    ADD CONSTRAINT user_in_room_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
